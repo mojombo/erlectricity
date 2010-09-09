15247c5ed152c93f65f97da10e3e46fb7191ac3f
Kelly McLaughlin <mclaughlin77@gmail.com>
2010-09-09 08:36:50 -0600

    Change require to load for test_helper

--- test/spec_suite.rb
+++ test/spec_suite.rb
@@ -1,2 +1,2 @@
-require File.dirname(__FILE__) + '/test_helper.rb'
-Dir[File.dirname(__FILE__) + '/*_spec.rb'].each{ |f| require f}
+load File.dirname(__FILE__) + '/test_helper.rb'
+Dir[File.dirname(__FILE__) + '/*_spec.rb'].each{ |f| load f}
